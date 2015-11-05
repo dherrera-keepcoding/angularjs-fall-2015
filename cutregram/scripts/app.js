bcaa6ac4894df14e55be23b1dfeea8d31df86bc7
Diego Herrera <dherrera.keepcoding@icloud.com>
2015-11-05 21:59:03 +0100

    AngularJS - Añadimos un paginador de Angular UI Bootstrap

--- cutregram/scripts/app.js
+++ cutregram/scripts/app.js
@@ -1,6 +1,6 @@
 
 // Definición de la aplicación.
-angular.module("cutregram", ["ngRoute", "angular-loading-bar"]);
+angular.module("cutregram", ["ngRoute", "angular-loading-bar", "ui.bootstrap"]);
 
 // En fase de config, inyectamos $httpProvider para configurar las cabeceras
 // por defecto de los distintos métodos HTTP del servicio $http, que usamos
--- cutregram/scripts/controllers/ColeccionPosts.js
+++ cutregram/scripts/controllers/ColeccionPosts.js
@@ -2,7 +2,36 @@
 // Controlador encargado de la lógica correspondiente con vista de colección de posts.
 angular.module("cutregram").controller("ColeccionPostsCtrl", function($scope, Posts, $location) {
 
-    $scope.posts = Posts.data;
+    // Almacenamos en local la colección de posts para que no se muestren
+    // todos directamente en la vista.
+    var posts = Posts.data;
+
+    // Establecemos las propiedades del paginador.
+    $scope.paginador = {
+
+        // Cambiamos de página.
+        cambioDePagina: function() {
+
+            // Obtenemos el primer y último elemento de la página a mostrar.
+            var primero = ($scope.paginador.paginaActual - 1) * $scope.paginador.elementosPorPagina;
+            var ultimo = primero + $scope.paginador.elementosPorPagina;
+
+            // Establecemos en la vista la página seleccionada.
+            $scope.posts = posts.slice(primero, ultimo);
+        },
+
+        // Página actual.
+        paginaActual: 1,
+
+        // Total de elementos -posts-.
+        totalElementos: posts.length,
+
+        // Tamaño de página.
+        elementosPorPagina: 5
+    };
+
+    // Forzamos el cambio de página para que traiga la primera al entrar a la vista.
+    $scope.paginador.cambioDePagina();
 
     // Redirigir el navegador al detalle del post indicado.
     $scope.navegar = function(idPost) {
